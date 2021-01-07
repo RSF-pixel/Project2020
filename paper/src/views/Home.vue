f87774aa3111e3146d1d736a48c74ba961f848e8
Cheap-Ship <56252676+Cheap-Ship@users.noreply.github.com>
2021-01-07 15:22:00 +0000

    Fixes

--- paper/src/views/Home.vue
+++ paper/src/views/Home.vue
@@ -1,18 +1,16 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div>
+    <SideBar/>
+    <h1>Home</h1>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
+import SideBar from "@/components/SideBar.vue";
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    SideBar
   }
 };
 </script>
