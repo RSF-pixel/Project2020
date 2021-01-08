84c4c19a2da64b40aac000c16bb6f177bc730334
Cheap-Ship <56252676+Cheap-Ship@users.noreply.github.com>
2021-01-08 16:09:51 +0000

    Store state

--- paper/src/store/index.js
+++ paper/src/store/index.js
@@ -5,7 +5,91 @@ Vue.use(Vuex);
 
 export default new Vuex.Store({
   state: {
-
+    route: "",
+    logged_user: 0,
+    estados: {
+      id_estado: 0,
+      estado: ""
+    },
+    tipo_utilizador: {
+      id_tipo: 0,
+      utilizador: ""
+    },
+    tipo_proposta: {
+      id_tipo: 0,
+      proposta: ""
+    },
+    utilizadores: {
+      id_utilizador: 0,
+      id_estado: 0,
+      nome: "",
+      apelido: "",
+      correio: "",
+      passe: "",
+      id_tipo: 0,
+      complementar: "",
+      foto: "",
+      cv: "",
+      portfolio: "",
+      facebook: "",
+      instagram: "",
+      github: "",
+      discord: "",
+      ano_letivo: ""
+    },
+    agenda: {
+      id_utilizador: 0,
+      id_convidado: 0,
+      dia: "",
+      hora: "",
+      detalhes: "",
+      ano_letivo: ""
+    },
+    propostas: {
+      id_proposta: 0,
+      id_estado: 0,
+      motivo: "",
+      id_criador: 0,
+      id_docente: 0,
+      id_tipo: 0,
+      titulo: "",
+      objetivos: "",
+      planos: "",
+      resultados: "",
+      perfil: "",
+      dados: "",
+      recursos: "",
+      data: "",
+      ano_letivo: ""
+    },
+    empresas: {
+      id_empresa: 0,
+      nome: "",
+      correio: "",
+      morada: "",
+      website: ""
+    },
+    estagios: {
+      id_proposta: 0,
+      id_empresa: 0,
+      nome_tutor: "",
+      contacto_tutor: "",
+      cargo_tutor: "",
+      correio_tutor: ""
+    },
+    inscricoes: {
+      id_utilizador: 0,
+      id_proposta: 0,
+      id_estado: 0,
+      preferencia: 0,
+      ano_letivo: ""
+    },
+    prazo: {
+      id_prazo: 0,
+      ano_letivo: "",
+      prazo: "",
+      dia_hora: ""
+    }
   },
   mutations: {
 
