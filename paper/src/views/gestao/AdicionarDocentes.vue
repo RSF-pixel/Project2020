ab07f457a0711022560e1dc9f75613d5cbde9585
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2021-01-09 09:35:20 +0000

    Adicionar docente sem VueX Stuff yet

--- paper/src/views/gestao/AdicionarDocentes.vue
+++ paper/src/views/gestao/AdicionarDocentes.vue
@@ -11,6 +11,27 @@
     >
     |
     <router-link :to="{ name: 'Prazos' }">Prazos</router-link>
+
+    <div>
+      <!-- FIXME:Probaly remove later with CSS -->
+      <br>
+      <br>
+      <!-- TODO: prevent.default the when everything is working -->
+      <form>
+        <label for="professores"></label>
+        <select name="professores" id="professoresID">
+          <option value="" disabled selected>Selecione o docente</option>
+          <!-- TODO: Make this work with Vue store  -->
+          <!-- For now the register is not ready yet to make that  -->
+          <!-- The idea is to have a getter that can filter every user that isn't a professor neither a member of an outside group -->
+          <option value="">Teste</option>
+        </select>
+        <div>
+          <br>
+          <input type="submit" value="Adicionar o docente" />
+        </div>
+      </form>
+    </div>
   </div>
 </template>
 
