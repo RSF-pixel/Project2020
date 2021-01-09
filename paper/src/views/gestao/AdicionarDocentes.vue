2c9fcd7006c9f8ddbe1d5fcf1900e88df86266ec
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2021-01-09 09:08:36 +0000

    Gestão Routes Basic template Done

---
+++ paper/src/views/gestao/AdicionarDocentes.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <SideBar />
+    <!-- Temporary REMOVE LATER -->
+    <h1>Adicionar Docente</h1>
+    <router-link :to="{ name: 'Aprovar' }">Aprovar</router-link> |
+    <router-link :to="{ name: 'Utilizadores' }">Utilizadores</router-link> |
+    <router-link :to="{ name: 'Inscricoes' }">Inscrições</router-link> |
+    <router-link :to="{ name: 'AdicionarDocentes' }"
+      >Adicionar Docente</router-link
+    >
+    |
+    <router-link :to="{ name: 'Prazos' }">Prazos</router-link>
+  </div>
+</template>
+
+<script>
+import SideBar from "@/components/SideBar.vue";
+export default {
+  components: {
+    SideBar,
+  },
+};
+</script>
