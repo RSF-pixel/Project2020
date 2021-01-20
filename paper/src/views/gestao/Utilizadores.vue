f88281e87b2c17ae5a47bcc570a2b1b421829cea
Cheap-Ship <56252676+Cheap-Ship@users.noreply.github.com>
2021-01-20 15:47:44 +0000

    Botões Tabela Utilizadores

--- paper/src/views/gestao/Utilizadores.vue
+++ paper/src/views/gestao/Utilizadores.vue
@@ -3,22 +3,16 @@
     <SideBar />
     <div class="area-principal anim-area-principal d-flex">
       <div class="area-conteudo">
-        <div
-          class="navegador-area-conteudo anim-sombra-area-conteudo d-flex justify-content-between fundo-f4 margem-b20 borda-r5 sombra-area-conteudo"
-        >
+        <div class="navegador-area-conteudo anim-sombra-area-conteudo d-flex justify-content-between fundo-f4 margem-b20 borda-r5 sombra-area-conteudo">
           <h1>Utilizadores</h1>
-          <router-link :to="{ name: 'Aprovar' }"
-            ><div><a>Aprovar</a></div></router-link
-          >
-          |
-          <router-link :to="{ name: 'Utilizadores' }"
-            ><div><a>Utilizadores</a></div></router-link
-          >|
-          <router-link :to="{ name: 'Inscricoes' }"
-            ><div><a>Inscrições</a></div></router-link>
-          |
-          <router-link :to="{ name: 'AdicionarDocentes' }"
-            ><div><a>Adicionar Docente</a></div></router-link>
+          <router-link :to="{ name: 'Aprovar' }">
+            <div><a>Aprovar</a></div></router-link> |
+          <router-link :to="{ name: 'Utilizadores' }">
+            <div><a>Utilizadores</a></div></router-link> |
+          <router-link :to="{ name: 'Inscricoes' }">
+            <div><a>Inscrições</a></div></router-link> |
+          <router-link :to="{ name: 'AdicionarDocentes' }">
+            <div><a>Adicionar Docente</a></div></router-link>
         </div>
         <div class="area-conteudo-se-navegador anim-sombra-area-conteudo d-flex justify-content-start fundo-f4 borda-r5 sombra-area-conteudo">
           <div>
@@ -43,7 +37,14 @@
                 <td>{{user.nome}}</td>
                 <td>{{user.correio}}</td>
                 <td>{{user.complementar}}</td>
-                <td><button @click="aprovarUtilizador(user.id)">Aprovar</button><button @click="negarUtilizador(user.id)">Negar</button></td>
+                <td>
+                  <template v-if="select == 'Docente'" >
+                  <button v-if="obterCCA(user.id)" @click="adicionarCCA(user.id)">Adicionar ao CCA</button>
+                  <button v-else @click="removerCCA(user.id)">Remover do CCA</button>
+                  </template>
+                  <button v-if="!obterBanido(user.id)" @click="banir(user.id)">Banir</button>
+                  <button v-else @click="reverter(user.id)">Reverter</button>
+                </td>
               </tr> 
             </table>
           </div>
@@ -67,6 +68,28 @@ export default {
   computed: {
     obterTabelaUsers() {
       return this.$store.getters.obterTabelaUsers(this.select);
+    }
+  },
+  methods: {
+    obterCCA(id) {
+      console.log(id)
+      return true;
+    },
+    obterBanido(id) {
+      console.log(id)
+      return false;
+    },
+    adicionarCCA(id) {
+      alert("adicionado " + id)
+    },
+    removerCCA(id) {
+      alert("removido " + id)
+    },
+    banir(id) {
+      alert("banido " + id)
+    },
+    reverter(id) {
+      alert("revertido " + id)
     }
   }
 };
