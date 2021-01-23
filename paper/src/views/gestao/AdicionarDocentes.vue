c9006b2f1aab33abdf5f19651c2a86f5fbea2caf
Rúben ferreira <56252824+Diamanterub@users.noreply.github.com>
2021-01-23 15:28:12 +0000

    Adicionar docentes HTML JS done only css missing

--- paper/src/views/gestao/AdicionarDocentes.vue
+++ paper/src/views/gestao/AdicionarDocentes.vue
@@ -27,37 +27,40 @@
           <div>
             <form @submit.prevent="register">
               <label for="firstName">Nome </label>
-              <input type="text" id="firstName" v-model="firstName" />
+              <input type="text" id="firstName" v-model="utilizador.primeiroNome" />
               <!-- FIXME:REMOVE LATER WITH CSS -->
               <br />
               <br />
               <label for="lastName">Apelido </label>
-              <input type="text" id="lastName" v-model="lastName" />
+              <input type="text" id="lastName" v-model="utilizador.apelido" />
 
               <!-- FIXME:REMOVE LATER WITH CSS -->
               <br />
               <br />
               <label for="email">Correio Eletrónico(institucional) </label>
-              <input type="email" id="email" v-model="email" />
+              <input type="email" id="email" v-model="utilizador.correio" />
 
               <!-- FIXME:REMOVE LATER WITH CSS -->
               <br />
               <br />
 
               <label for="password">Palavra-passe </label>
-              <input type="password" id="password" v-model="password" />
+              <input type="password" id="password" v-model="utilizador.password" />
               <!-- FIXME:REMOVE LATER WITH CSS -->
               <br />
               <br />
 
-              <input type="checkbox" id="ccaCheck" v-model="ccaCheck" />
+              <input type="checkbox" id="ccaCheck" v-model="utilizador.ccaCheck" />
               <label for="ccaCheck">Membro da CCA </label>
 
-              <!-- FIXME:REMOVE LATER WITH CSS -->
               <br />
               <br />
 
               <input type="submit" value="Criar o docente" />
+              <!-- FIXME:REMOVE LATER WITH CSS -->
+              <br>
+
+              <h3>{{this.estado}}</h3>
             </form>
           </div>
         </div>
@@ -75,11 +78,20 @@ export default {
   data()
   {
     return{
-      firstName: "",
-      lastName:"",
-      email:"",
+      estado:"",
+      utilizador:{
+      primeiroNome: "",
+      apelido:"",
+      correio:"",
       password:"",
       ccaCheck: false,
+      tipo_utilizador: null,
+      numero_estudante: null,
+      nome_empresa: null,
+      correio_empresa: null,
+      morada_empresa: null,
+      website_empresa: null
+      }
     }
 
   },
@@ -87,15 +99,45 @@ export default {
     //Metodo do registo do docente
     register()
     {
-      // FIXME: Pedro eu nao sei se queres fazer uma entidade no state separada para os docentes como fizeste para entidades externas
+      let utilizador = {
+          id_utilizador: this.$store.getters.proximoIDUtilizador,
+          id_estado: 1,
+          nome: this.utilizador.primeiroNome,
+          apelido: this.utilizador.apelido,
+          correio: this.utilizador.correio,
+          passe: this.utilizador.password,
+          id_tipo: 0,
+          numero_estudante: null,
+          nome_empresa: null,
+          foto: "https://lh3.googleusercontent.com/-4yFaWmS7-Pg/X_xzGKwqwHI/AAAAAAAAAAY/L78mg1HQzvELjdvv5xiLqZT6keuBmoGSACMICGAYYCw/s83-c/foto_default.png",
+          inscricao: null,
+          cv: null,
+          portfolio: null,
+          facebook: null,
+          instagram: null,
+          github: null,
+          discord: null,
+          ano: ""
+        }
+        const empresa = this.utilizador.tipo_utilizador == 2 ? {
+          nome: this.utilizador.nome_empresa,
+          correio: this.utilizador.correio_empresa,
+          morada: this.utilizador.morada_empresa,
+          website: this.utilizador.website_empresa,
+        } : null;
+        const dados = {
+          utilizador: utilizador,
+          empresa: empresa
+        }
+        try {
 
-
+          this.$store.dispatch("registo", dados)
+          this.estado = "Docente Criado com sucesso"
+        }
+        catch(error){
+          this.estado = "Erro a criar o docente"                                     
+        }
     }
-
-
-
-
-
 
   }
   
